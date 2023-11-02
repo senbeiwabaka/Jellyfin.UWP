b84f15a13c704e8d61e8928bac1cd6f723dc3411
Michael Yahner <MichaelYahner@outlook.com>
2023-11-02 11:29:08 -0400

    update

---
+++ tests/Jellyfin.UWP.Tests/DetailsPageTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jellyfin.UWP.Tests
+{
+    public sealed class DetailsPageTests
+    {
+        [UITestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
