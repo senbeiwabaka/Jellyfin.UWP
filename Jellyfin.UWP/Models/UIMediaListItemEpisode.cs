e5e5c792053500e2b1764518341625d48d87e9fd
Michael Yahner <MichaelYahner@outlook.com>
2023-12-09 23:25:04 -0500

    more ui updates

--- Jellyfin.UWP/Models/UIMediaListItemEpisode.cs
+++ Jellyfin.UWP/Models/UIMediaListItemEpisode.cs
@@ -5,5 +5,7 @@
         public string SeriesName { get; internal set; }
 
         public string Description { get; internal set; }
+
+        public int UnPlayedCount { get; internal set; }
     }
 }
